8109d972bd9a29a0acc908178e6fe83f11847a3d
vzx151 vzx151 <vzx151@ku.dk>
2019-02-18 09:49:56 +0100

    Add sphinx setuptools command

--- setup.py
+++ setup.py
@@ -3,4 +3,12 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True)
+    setuptools.setup(
+        name="sphinx-runner",
+        author="Jakob Jul Elben",
+        author_email="elbenjakobjul@gmail.com",
+        version="0.1.0",
+        description="A sphinx-runner for setuptools",
+        py_modules=['sphinx_runner'],
+        install_requires=['sphinx'],
+    )
